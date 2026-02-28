de1b34c94484a17e0280086583daf1daae2b672f
WalnutBai <Walnuthyper@foxmail.com>
2026-02-28 13:01:57 +0800

    Bug修复

--- src/config.ts
+++ src/config.ts
@@ -23,7 +23,7 @@ const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UT
 export const siteConfig: SiteConfig = {
 	title: "琉璃亭",
 	subtitle: "✿白箬璃❀ᵒᵏᵎ的个人博客",
-	siteURL: "https://walnuthyper.pages.dev/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://walnutbai.dpdns.org/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2026-01-01", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
